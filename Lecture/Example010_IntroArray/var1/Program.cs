885aa19c39ff1d8d12206b50fbbf589dc939e366
Anton Vorontsov <antvork@gmail.com>
2023-02-26 22:36:08 +0700

    add Example009-12 out Lectere2

---
+++ Lecture/Example010_IntroArray/var1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Найти максимальное из 9 чисел
+//Решение через функцию  и через массив
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2> result) result =arg2;
+    if (arg3> result) result =arg3;
+    return result;
+    
+}
+
+int [] array = {21,32,34,45,56,67,87,89,59}; //Создаем массив
+array[0] = 12; //Присвоение значения массиву
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
---
+++ Lecture/Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection) //В аргументы мы должны вставить массив //Данный метод заполняет массив числами от 1 до 9
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //Данный метод распечатывает массив на новой строке пока есть данные  в массиве
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int [] array = new int[10]; // создание массива с 10 элементами заполненый 0
+
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = Index0f(array, 9);
+Console.WriteLine(pos);
