cb0b4e0340ad40cbd4be60b0088338dac2fec9ca
Anton Vorontsov <antvork@gmail.com>
2023-02-27 12:01:37 +0700

    add Homework001 Task1

---
+++ Homework/Homework_001/Task001/Ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 10;
+int max = 0;
+if(a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+Console.Write("max=");
+Console.WriteLine(max);
---
+++ Homework/Homework_001/Task001/Ex03/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = -9;
+int b = -3;
+int max = 0;
+if(a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+Console.Write("max=");
+Console.WriteLine(max);
