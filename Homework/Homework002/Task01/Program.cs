07f5c84233cde0aabc64074046c31ca80f42b650
Anton Vorontsov <antvork@gmail.com>
2023-03-01 15:17:52 +0700

    edited README

--- Homework/Homework002/Task01/Program.cs
+++ Homework/Homework002/Task01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int hundred = 100;
+int ten = 10;
+int result;
+if (number > 99 && number < 1000)
+{
+    result = (number % hundred) / ten;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
