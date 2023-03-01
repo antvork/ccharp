daad4dc75bef5044a09c135bbcb35e6ae6944d8d
Anton Vorontsov <antvork@gmail.com>
2023-03-01 18:55:19 +0700

    add HW02 task3

---
+++ Homework/Homework002/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int day = GetNumber("Введите число дня недели: ");
+string output = ("Не верное число");
+
+if(day <= 7 && day > 0)
+{
+    if(day>5)
+    {
+        output = ("Да");
+    }
+    else
+    output = ("Нет");
+}
+Console.WriteLine(output);
