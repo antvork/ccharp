eb39ef7a852f40cf57a4ce3cbe7abffc44878bb1
Anton Vorontsov <antvork@gmail.com>
2023-03-03 16:05:22 +0700

    add Homework03 task01

--- Homework/Homework003/Task01/Program.cs
+++ Homework/Homework003/Task01/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void CheckPolindrome(int num)
+{
+    int numA = 0;
+    int numB = 0;
+    int numC = 0;
+    if (num > 9999 && num < 100000)
+    {
+        numA = num / 1000;
+        numB = num % 100;
+        numC = numA / 10 + ((numA % 10) * 10);
+        if (numB == numC)
+        {
+            Console.WriteLine("Число полиндром");
+        }
+        else
+        {
+            Console.WriteLine("Число не полиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Число не соответсвует парраметрам");
+    }
+}
+
+int number = GetNumber("Введите пятизначное число");
+CheckPolindrome(number);
+
+
