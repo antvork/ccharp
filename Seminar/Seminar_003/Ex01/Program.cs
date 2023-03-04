47671a977f1e5ecdbed1a6ccf35021fe2c93e350
Anton Vorontsov <antvork@gmail.com>
2023-03-04 09:48:03 +0700

    Добавил в Seminar3 Ex1 рещение задачи 24 с Семинара3

--- Seminar/Seminar_003/Ex01/Program.cs
+++ Seminar/Seminar_003/Ex01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти кубы чисел от 1 до N (24)
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string GetResult(int n)
+{
+    int index = 1;
+    string result = String.Empty; //Присвоение пустой строки (также как для числа)
+    while (index <= n)
+    {
+        int sqr = index * index * index;
+        result = result + "[" + index + "==>" + sqr + "] "; //Контатинация строк( тут в результат добавляется еще строки)
+        index++;
+    }
+    return result;
+}
+int x = GetNumber("Введите число: ");
+Console.WriteLine(GetResult(x));
