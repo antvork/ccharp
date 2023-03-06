637b152ee5b28ea16a98458d3ae52808a0a0db82
Anton Vorontsov <antvork@gmail.com>
2023-03-06 12:32:50 +0700

    add HomeWork003 task3

--- Homework/Homework003/Task03/Program.cs
+++ Homework/Homework003/Task03/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите метод(-ы), который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string GetSqrNumber(int number)
+{
+    double index = 1;
+    string result = String.Empty;
+    while (index <= number)
+    {
+        double sqr = Math.Pow(index, 3);
+        result = result + "[" + index + "==>" + sqr + "] ";
+        index++;
+    }
+    return result;
+}
+
+int n = GetNumber("Введите число: ");
+Console.WriteLine(GetSqrNumber(n));
+
