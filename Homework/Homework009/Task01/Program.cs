0937e5472aa337baada281eb07111b4bf572b5d9
Anton Vorontsov <antvork@gmail.com>
2023-03-28 11:20:56 +0700

    add solution all task  HW09

---
+++ Homework/Homework009/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string AllNumberN(int n)
+{
+    string result = String.Empty;
+    if (n == 1) return result = result + ($"{1}");
+    else return result = ($"{n}, ") + ($"{AllNumberN(n - 1)} ");
+}
+
+int number = GetNumber("Введите число: ");
+Console.WriteLine(AllNumberN(number));
---
+++ Homework/Homework009/Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumFromMtoN(int m, int n)
+{
+    int sum = 0;
+    if (m > n)
+    {
+        if (m == n) return sum = sum + m;
+        else return sum = m + SumFromMtoN(m - 1, n);
+    }
+    else
+    {
+        if (n == m) return sum = sum + n;
+        else return sum = n + SumFromMtoN(m, n - 1);
+    }
+}
+
+int m = GetNumber("Задайте значение M: ");
+int n = GetNumber("Задайте значение N: ");
+int result = SumFromMtoN(m, n);
+Console.WriteLine(result);
